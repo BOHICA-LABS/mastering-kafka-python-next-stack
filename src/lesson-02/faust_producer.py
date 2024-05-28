8484b67d7d23e9a3953cd2211998f97a4dc68588
Joshua Magady <josh.magady@gmail.com>
2024-05-27 22:44:12 -0500

    feat: Add Faust producer for user data
    A new Faust producer, myapp_producer, has been implemented in the src/lesson-02/faust_producer.py file. This producer generates a new User every second and sends it to the 'users' topic on a Kafka broker running on localhost. User data is serialized using Protobuf before being sent.

---
+++ src/lesson-02/faust_producer.py
@@ -0,0 +1,22 @@
+import faust
+from protobuf.protos.user_pb2 import User
+from protobuf.serializers.protobufcodec import ProtobufCodec
+from google.protobuf.message import Message
+from typing import Any
+
+
+user_codec = ProtobufCodec(User)
+
+app = faust.App('myapp_producer', broker='kafka://localhost:9092', web_port=6067)
+topic = app.topic('users', value_type=bytes, key_serializer=user_codec, value_serializer=user_codec)
+
+
+@app.timer(interval=1.0)
+async def produce():
+    user = User(name='John Doe', age=30, email='john@example.com', interests=['hiking', 'reading'])
+    await topic.send(value=user)
+    print(f'Sent user: {user.name}, {user.age}, {user.email}, {user.interests}')
+
+
+if __name__ == '__main__':
+    app.main()
